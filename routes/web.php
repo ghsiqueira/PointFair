491f6f4036e9e24060bc1e52271bce3a196f1312
ghsiqueira <gabrielh2805@gmail.com>
2023-05-15 21:22:48 -0300

    updt

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [EventController::class, 'index']);
-Route::get('/feiras/create', [EventController::class, 'create']);
-Route::get('/feiras/{id}', [EventController::class, 'show']);
-Route::post('/feiras', [EventController::class, 'store']);
-Route::delete('/feiras/{id}', [EventController::class, 'destroy']);
-
+Route::get('/', function () { return view('inicio'); })->name('inicio');
 Route::get('/sobrenos', function () { return view('aboutus'); })->name('aboutus');
 Route::get('/duvidasFrequentes', function () { return view('duvidasFrequentes'); })->name('duvidasFrequentes');
 Route::get('/contato', function () { return view('contact'); })->name('contact');
-Route::get('/home', function () { return view('inicio'); })->name('inicio');
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
