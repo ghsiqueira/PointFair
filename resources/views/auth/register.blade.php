c23017315a0d16b943b0e43fd97ea7d346a82a69
ghsiqueira <gabrielh2805@gmail.com>
2023-05-17 18:10:55 -0300

    update css e js

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,58 +1,71 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ secure_asset('public\build\assets\app-96afdf9a.css') }}">
+</head>
+<body>
+    <x-guest-layout>
+        <x-authentication-card>
+            <x-slot name="logo">
+                <x-authentication-card-logo />
+            </x-slot>
 
-        <x-validation-errors class="mb-4" />
+            <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('register') }}" onsubmit="return validateTerms()">
-            @csrf
+            <form method="POST" action="{{ route('register') }}" onsubmit="return validateTerms()">
+                @csrf
 
-            <div>
-                <x-label for="name" value="{{ __('Nome:') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+                <div>
+                    <x-label for="name" value="{{ __('Nome:') }}" />
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email:') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="email" value="{{ __('Email:') }}" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Senha:') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="password" value="{{ __('Senha:') }}" />
+                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirmar Senha:') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
+                <div class="mt-4">
+                    <x-label for="password_confirmation" value="{{ __('Confirmar Senha:') }}" />
+                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                </div>
 
-            <div class="mt-4">
-                <x-label for="terms">
-                    <div class="flex items-center">
-                        <x-checkbox name="terms" id="terms" required />
+                <div class="mt-4">
+                    <x-label for="terms">
+                        <div class="flex items-center">
+                            <x-checkbox name="terms" id="terms" required />
 
-                        <div class="ml-2">
-                            {!! __('Estou de acordo com os :terms_of_service e com a :privacy_policy.', [
-                                    'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Termos de Serviço').'</a>',
-                                    'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Política de Privacidade').'</a>',
-                            ]) !!}
+                            <div class="ml-2">
+                                {!! __('Estou de acordo com os :terms_of_service e com a :privacy_policy.', [
+                                        'terms_of_service' => '<a target="_blank" href="'.route('terms.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Termos de Serviço').'</a>',
+                                        'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">'.__('Política de Privacidade').'</a>',
+                                ]) !!}
+                            </div>
                         </div>
-                    </div>
-                </x-label>
-            </div>
+                    </x-label>
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Já Registrado?') }}
-                </a>
+                <div class="flex items-center justify-end mt-4">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                        {{ __('Já Registrado?') }}
+                    </a>
 
-                <x-button class="ml-4">
-                    {{ __('Registrar') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+                    <x-button class="ml-4">
+                        {{ __('Registrar') }}
+                    </x-button>
+                </div>
+            </form>
+        </x-authentication-card>
+    </x-guest-layout>
+    <script src="public\build\assets\app-b9a1a811.js"></script>
+</body>
+</html>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,7 @@
         
         <!-- CSS e JS da aplicação -->
         <link rel="stylesheet" href="{{ secure_asset('css/styles.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('css/styles.css') }}"> -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" class="rel">
     </head>
     <body>
