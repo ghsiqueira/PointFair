a4d819acf073b6a2ee77cfef550f6ba351c16a87
ghsiqueira <gabrielh2805@gmail.com>
2023-05-18 17:00:40 -0300

    update

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -65,35 +65,26 @@
                 </div>
             </nav>
         </header>
-        <main>
-            <div class="container-fluid">
-                <div class="row">
-                    @if(session('msg'))
-                    <p class="msg">{{ session('msg') }}</p>
-                    @endif
-                </div>
-                <div class="mainIndex">
-                    <div class="ScrenbnMain">
-                        <div class="TextInfo">
-                            <h1 class="mainPointFair">PointFair</h1>
-                            <h2>Compre consciente, valorize o autônomo: <br> Juntos por um comércio justo e diverso!</h2>
-                            <p>O Point Fair tem o objetivo de divulgar feirantes.</p>
-                            <div class="buttons">
-                                <a href="">
-                                    <button class="btn btn-primary">Baixar App</button>
-                                </a>
-                                <!-- <a href="/sobrenos" class="saiba-mais">Saiba mais
-                                    <span class="iconify" data-icon="eva:arrow-ios-forward-fill" data-height="22"></span>
-                                </a> -->
-                            </div>
-                        </div>
-                        <div class="ImgHome">
-                            <img src="img/logo.png" alt="Logo">
-                        </div>
+        <div class="mainIndex">
+            <div class="ScrenbnMain">
+                <div class="TextInfo">
+                    <h1 class="mainPointFair">PointFair</h1>
+                    <h2>Compre consciente, valorize o autônomo: <br> Juntos por um comércio justo e diverso!</h2>
+                    <p>O Point Fair tem o objetivo de divulgar feirantes.</p>
+                    <div class="buttons">
+                        <a href="">
+                            <button class="btn btn-primary">Baixar App</button>
+                        </a>
+                        <!-- <a href="/sobrenos" class="saiba-mais">Saiba mais
+                            <span class="iconify" data-icon="eva:arrow-ios-forward-fill" data-height="22"></span>
+                        </a> -->
                     </div>
                 </div>
+                <div class="ImgHome">
+                    <img src="img/logo.png" alt="Logo">
+                </div>
             </div>
-        </main>
+        </div>
         <footer class="Rodape">
             <div class="container">
                 <div class="row">
