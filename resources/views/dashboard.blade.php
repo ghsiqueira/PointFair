7fd6de524f1a0eb1ba6dfeaeca515848fcd2113c
ghsiqueira <gabrielh2805@gmail.com>
2023-05-18 16:00:42 -0300

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -125,7 +125,6 @@
                   <p class="post-date">Publicado em 23 de Abril de 2023</p>
                 </div>
               </div>
-              @livewire('infinite-scroll')
             </div>
           </div>
         </div>
