3e6b124fab0b48fe469ab4c6a2d50880950b54eb
ghsiqueira <gabrielh2805@gmail.com>
2023-05-18 17:25:26 -0300

    updt

--- resources/views/duvidasFrequentes.blade.php
+++ resources/views/duvidasFrequentes.blade.php
@@ -18,6 +18,8 @@
         <link rel="stylesheet" href="{{ secure_asset('css/animacoes.css') }}">
         <link rel="stylesheet" href="{{ secure_asset('css/duvidasfrequentes.css') }}">
         <link rel="stylesheet" href="{{ secure_asset('css/inicio.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/contato.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/sobrenos.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" class="rel">
     </head>
     <body>
