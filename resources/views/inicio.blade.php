56fb0d2089922cbeca8ee2f9a17bada64bf10875
ghsiqueira <gabrielh2805@gmail.com>
2023-05-18 17:12:43 -0300

    atualização

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -17,7 +17,6 @@
         <link rel="stylesheet" href="{{ secure_asset('css/navfooter.css') }}">
         <link rel="stylesheet" href="{{ secure_asset('css/animacoes.css') }}">
         <link rel="stylesheet" href="{{ secure_asset('css/inicio.css') }}">
-        <link rel="stylesheet" href="{{ secure_asset('css/feed.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" class="rel">
     </head>
     <body>
